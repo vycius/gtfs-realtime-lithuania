c6d7338473c986704b37435e853b5672203214de
Karolis Vyčius <k.vycius@gmail.com>
2022-03-04 15:24:07 +0200

    First commit

---
+++ combiner.py
@@ -0,0 +1,61 @@
+import shutil
+from os import listdir, path
+
+import pandas as pd
+
+temp_directory = 'data/temp/'
+
+
+def write_permanent_vehicle_positions(file_path: str, df: pd.DataFrame):
+    sorted_df = df.sort_values(
+        by=[
+            'Transportas',
+            'Marsrutas',
+            'ReisoID',
+            'MasinosNumeris',
+            'MasinosTipas',
+            'MatavimoLaikas',
+            'Gauta',
+        ],
+    )
+    sorted_df.to_parquet(file_path, index=False)
+
+    print(sorted_df.info())
+
+
+def combine_vehicle_position_files(file_name: str, file_path: str):
+    new_df = pd.read_parquet(file_path)
+    permanent_file_path = f'data/vehicle_positions/vilnius/{file_name}'
+
+    if not path.exists(permanent_file_path):
+        write_permanent_vehicle_positions(file_path, new_df)
+    else:
+        previous_df = pd.read_parquet(file_path)
+        combined_df = pd.concat([previous_df, new_df], axis=0) \
+            .sort_values(by=['MatavimoLaikas', 'Gauta']) \
+            .drop_duplicates(
+            subset=[
+                'Transportas',
+                'Marsrutas',
+                'ReisoID',
+                'MasinosNumeris',
+                'MasinosTipas',
+                'MatavimoLaikas',
+            ]
+        )
+
+        write_permanent_vehicle_positions(file_path, combined_df)
+
+
+def combine_vehicle_position_files_in_directory():
+    for filename in listdir(temp_directory):
+        file_path = path.join(temp_directory, filename)
+
+        if path.isfile(file_path):
+            combine_vehicle_position_files(filename, file_path)
+
+    shutil.rmtree(temp_directory)
+
+
+if __name__ == '__main__':
+    combine_vehicle_position_files_in_directory()
---
+++ fetcher.py
@@ -0,0 +1,82 @@
+import os
+import time
+from datetime import datetime, timedelta
+
+import pandas as pd
+
+
+def fetch_vehicle_positions_df() -> pd.DataFrame:
+    df = pd.read_csv(
+        'http://www.stops.lt/vilnius/gps_full.txt',
+        usecols=[
+            'Transportas',
+            'Marsrutas',
+            'ReisoID',
+            'MasinosNumeris',
+            'Ilguma',
+            'Platuma',
+            'Greitis',
+            'Azimutas',
+            'ReisoPradziaMinutemis',
+            'NuokrypisSekundemis',
+            'MatavimoLaikas',
+            'MasinosTipas',
+        ],
+        dtype={
+            'Transportas': pd.StringDtype(),
+            'Marsrutas': pd.StringDtype(),
+            'MasinosNumeris': pd.StringDtype(),
+            'ReisoID': pd.Int64Dtype(),
+            'NuokrypisSekundemis': pd.Int64Dtype(),
+            'ReisoPradziaMinutemis': pd.Int64Dtype(),
+            'MasinosTipas': pd.CategoricalDtype(),
+        }
+    )
+
+    df['Lat'] = df['Platuma'] / 1000000
+    df['Lon'] = df['Ilguma'] / 1000000
+
+    return df
+
+
+def parse_and_listen_for_vehicle_positions(started_at: datetime) -> pd.DataFrame:
+    df_unique_vehicle_positions = None
+
+    while datetime.now() - started_at < timedelta(hours=1, minutes=10):
+        fetched_at = int(datetime.timestamp(datetime.now()))
+        df_new_vehicle_positions = fetch_vehicle_positions_df()
+
+        df_new_vehicle_positions['Gauta'] = fetched_at
+
+        if df_unique_vehicle_positions is None:
+            df_unique_vehicle_positions = df_new_vehicle_positions
+
+        df_unique_vehicle_positions = pd.concat([df_unique_vehicle_positions, df_new_vehicle_positions]) \
+            .drop_duplicates(
+            subset=[
+                'Transportas',
+                'Marsrutas',
+                'ReisoID',
+                'MasinosNumeris',
+                'MasinosTipas',
+                'MatavimoLaikas',
+            ]
+        )
+
+        time.sleep(3)
+
+    return df_unique_vehicle_positions
+
+
+def fetch_vehicle_positions():
+    started_at = datetime.now()
+    df_vehicle_positions = parse_and_listen_for_vehicle_positions(started_at)
+
+    file_date = (started_at - timedelta(hours=3, minutes=59)).date().isoformat()
+
+    os.makedirs('data/temp', exist_ok=True)
+    df_vehicle_positions.to_parquet(f'data/temp/{file_date}.parquet')
+
+
+if __name__ == '__main__':
+    fetch_vehicle_positions()
